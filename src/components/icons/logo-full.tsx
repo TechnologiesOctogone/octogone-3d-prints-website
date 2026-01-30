a34a090bd26ad8d0eae971e5092401f739830b4f
Alexandre Hallaine <alexandre@hallaine.com>
2026-01-30 00:20:16 +0000

    feat(icons): logo full

---
+++ src/components/icons/logo-full.tsx
@@ -0,0 +1,35 @@
+import type { SVGProps } from "react";
+
+export const LogoFull = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    role="img"
+    viewBox="0 0 2076 733"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={115}
+    {...props}
+  >
+    <title>Octogone 3D Prints Logo</title>
+    <path d="M62.2 415.6 119.7 256.6 273 184.7 432 242.2 503.8 395.4 446.3 554.4 293.1 626.3 134.1 568.8Z" />
+    <text
+      x={613.3}
+      y={370}
+      stroke="none"
+      fill="currentColor"
+      fontFamily="Aptos, sans-serif"
+      fontSize={303}
+    >
+      Octogone
+    </text>
+    <text
+      x={640.8}
+      y={600}
+      stroke="none"
+      fill="currentColor"
+      fontFamily="Grandview, sans-serif"
+      fontSize={165}
+    >
+      3D Prints
+    </text>
+  </svg>
+);
--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,13 +1,15 @@
-import { SVGProps } from "react";
+import type { SVGProps } from "react";
 
 export const Logo = (props: SVGProps<SVGSVGElement>) => (
   <svg
+    role="img"
     viewBox="0 0 566 567"
     fill="none"
     stroke="currentColor"
     strokeWidth={115}
     {...props}
   >
+    <title>Octogone 3D Prints Logo</title>
     <path d="m62 294 57-159 153-72 159 58 72 153-58 159-153 72-159-58Z" />
   </svg>
 );
