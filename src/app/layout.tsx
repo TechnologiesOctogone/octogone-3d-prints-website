75c4d341f2616e6e151c6deed125dfd152eb562d
Alexandre Hallaine <alexandre@hallaine.com>
2026-03-07 07:47:25 -0500

    feat(layout/button): move in component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ChevronsRight } from "lucide-react";
+import { ChevronsRight, Menu } from "lucide-react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
@@ -7,8 +7,20 @@ import { LogoFull } from "@/components/icons/logo-full";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Logo } from "@/components/icons/logo";
-import { Navigation } from "@/components/navigation";
 import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const _inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -18,9 +30,28 @@ export const metadata: Metadata = {
     "Services d'impression 3D de haute qualité pour vos projets personnels et professionnels.",
 };
 
+export function Action() {
+  return (
+    <Button
+      nativeButton={false}
+      render=<Link href="mailto:info@octogone3dprints.com" />
+    >
+      Imprimez
+      <ChevronsRight />
+    </Button>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const links = [
+    { href: "/#about", label: "À propos" },
+    { href: "/#services", label: "Services" },
+    { href: "/#projects", label: "Projets" },
+    { href: "/#contact", label: "Contact" },
+  ];
+
   return (
     <html
       lang="en"
@@ -34,21 +65,51 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <nav className="fixed w-full z-50 bg-background border-b">
+          <header className="fixed w-full z f-50 bg-background border-b">
             <div className="container mx-auto h-16 flex justify-between items-center">
               <Link href="/">
                 <LogoFull className="h-12" />
               </Link>
-              <Navigation />
-              <Button
-                nativeButton={false}
-                render=<Link href="mailto:info@octogone3dprints.com" />
-              >
-                Imprimez
-                <ChevronsRight />
-              </Button>
+              <NavigationMenu className="hidden md:flex">
+                <NavigationMenuList className="gap-3">
+                  {links.map((link) => (
+                    <NavigationMenuItem key={link.href}>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                        render={<Link href={link.href} />}
+                      >
+                        {link.label}
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+
+              <div className="md:hidden">
+                <Sheet>
+                  <SheetTrigger render={<Button variant="ghost" size="icon" />}>
+                    <Menu />
+                  </SheetTrigger>
+                  <SheetContent showCloseButton={false}>
+                    <div className="grid gap-6 p-4">
+                      {links.map((link) => (
+                        <Button
+                          key={link.href}
+                          render={<Link href={link.href} />}
+                        >
+                          {link.label}
+                        </Button>
+                      ))}
+                    </div>
+                    <SheetFooter>
+                      <Action />
+                    </SheetFooter>
+                  </SheetContent>
+                </Sheet>
+              </div>
+              <Action />
             </div>
-          </nav>
+          </header>
 
           <div className="h-16" />
           {children}
